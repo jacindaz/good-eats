abf710c3850623d80f4fdfe4e9ea224177b98827
Jacinda Zhong <jacindaz@gmail.com>
2014-06-22 08:44:22 -0400

    Creates restaurants and reviews tables, db config, and routes

---
+++ db/migrate/20140622122729_create_restaurant_table.rb
@@ -0,0 +1,14 @@
+class CreateRestaurantTable < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name, null: false
+      t.string :address, null: false
+      t.string :city, null: false
+      t.string :state, null: false
+      t.integer :zipcode, null: false
+      t.text :description
+      t.string :category
+      t.timestamps
+    end
+  end
+end
